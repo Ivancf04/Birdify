6bc53030e79495ab57437ac9a0caa005ec62a27c
sasa71928 <osalazarsalas@gmail.com>
2025-12-16 00:53:53 -0700

    transiciones parte 2.2

--- Birdify/components/ui/FadeView.tsx
+++ Birdify/components/ui/FadeView.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { Animated, StyleProp, ViewStyle, View } from "react-native";
+import { Animated, StyleProp, ViewStyle } from "react-native";
 
 interface FadeViewProps {
   visible: boolean;
@@ -12,18 +12,23 @@ export const FadeView: React.FC<FadeViewProps> = ({
   visible, 
   children, 
   style, 
-  duration = 520 
+  duration = 250 
 }) => {
   const fadeAnim = useRef(new Animated.Value(visible ? 1 : 0)).current;
 
   useEffect(() => {
     if (visible) {
+      // Forzar valor a 0 antes
       fadeAnim.setValue(0);
+      
       Animated.timing(fadeAnim, {
         toValue: 1,
         duration: duration,
         useNativeDriver: true,
       }).start();
+    } else {
+      // Al ocultar, reset a 0
+      fadeAnim.setValue(0);
     }
   }, [visible, duration]);
 
